cab19939ad006077d25f9ff6a0dabe737f8c192a
Ashley Martinez <ashley.martinez32@gmail.com>
2022-12-05 15:15:45 -0600

    working on stats

--- script/card.js
+++ script/card.js
@@ -6,20 +6,26 @@ const moveOne = document.querySelector('#move-one');
 const moveTwo = document.querySelector('#move-two');
 const moveThree = document.querySelector('#move-three');
 const pokeImage = document.querySelector('#poke-img');
+const hp = document.querySelector('#hp');
 
 
-//making fetch to pokeapi
+//      making fetch to pokeapi
 const fetchPokeApi = async (pokeName) => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokeName);
     const pokeData = await response.json();
-// get that data back as json
+//      get that data back as json
     return pokeData;
 }
 
-//event listener for search bar
+//      event listener for search bar
 search.addEventListener('change', async (event) => {
     const pokeData = await fetchPokeApi(event.target.value)
     console.log(pokeData)
+    console.log(pokeData.stats[5].base_stat)
+
+//    sets HP
+    hp.innerHTML = pokeData.stats[5].base_stat + ' HP'
+
 
 //    sets pokemon id
     number.innerHTML = '#'+ pokeData.id.toString().padStart(3, '0');
